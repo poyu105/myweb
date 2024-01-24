5e2cf65543bd73dc7db2ad037d0842dcec56e8dd
poyu105 <chen06115@gmail.com>
2024-01-24 20:59:53 +0800

    update with json

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -3,4 +3,33 @@ $(document).ready(function () {
         e.preventDefault();
         $('.navbar').toggleClass('show-menu');
     });
+
+    getData('all');
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        getData($(this).val());
+    });
 });
+function getData(val){
+    $.ajax({
+        type: "get",
+        url: "./portfolio.json",
+        data: "",
+        dataType: "json",
+        success: function (data) {
+            //初始化post
+            $('.post').empty();
+
+            if(val == 'all'){
+                $.each(data.portfolios, function (i, value) { 
+                    console.log("loop on : "+data.portfolios[i]["name"]);
+                    let title = $('<div>').addClass('title').text(data.portfolios[i]["name"]);
+                    let describe = $('<div>').addClass('describe').text(data.portfolios[i]["describe"]);
+                    let image = $('<img>').addClass('cover-img').attr("src", data.portfolios[i]["image"]);;
+                    $('.post').append(image);
+                });
+            }
+        }
+    });
+    console.log("btn value = "+val);
+}
