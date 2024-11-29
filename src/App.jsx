de4376fc359da605fb9e1335093c516296c8144c
poyu <chen06115@gmail.com>
2024-11-30 00:30:32 +0800

    修正base路由

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Skill from './pages/Skill'
+import { Router } from 'react-router-dom'
 
 function App() {  
   return (
-    <div>
-      <Navbar/>
-      <div className='text-md lg:mx-56 md:mx-36 sm:mx-16 mx-6 my-8 bg-slate-300'>
-        <Home/>
-        <About/>
-        <Skill/>
+    <Router basename='/myweb'>
+      <div>
+        <Navbar/>
+        <div className='text-md lg:mx-56 md:mx-36 sm:mx-16 mx-6 my-8 bg-slate-300'>
+          <Home/>
+          <About/>
+          <Skill/>
+        </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
+    </Router>
   )
 }
 
