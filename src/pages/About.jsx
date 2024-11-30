070220805eea1f1383872b92bc3b3a3d54b6f1d6
poyu <chen06115@gmail.com>
2024-11-30 19:21:08 +0800

    新增懸浮標題，修改文字顏色

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About(){
     return(
         <section id="about" className="mt-2">
-            <h1 className="text-4xl text-center border-t-2 border-b-2">關於我</h1>
+            <h1 className="text-4xl text-center border-t-2 border-b-2 sticky top-0 bg-slate-800">關於我</h1>
             <div className="flex items-center gap-5 p-2">
                 <img className="w-48 h-auto rounded" alt="myPhoto" src="./image/myPhoto.jpg"></img>
                 <p>
