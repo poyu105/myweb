5ee8b4f2d94366d72ed875b73a314ebab3c84684
poyu <chen06115@gmail.com>
2024-12-06 12:42:19 +0800

    新增react輪播套件並修正RWD樣式

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Skill from './pages/Skill'
-import Protfolio from './pages/Protfolio'
+import Portfolio from './pages/Portfolio'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 
 function App() {  
   return (
     <div className="h-screen flex flex-col">
       <Navbar/>
-      <div className='lg:text-lg md:text-base text-sm lg:mx-56 md:mx-36 sm:mx-16 mx-6 my-8 text-slate-200 overflow-y-scroll'>
+      <div className='lg:text-lg md:text-base text-sm lg:mx-56 md:mx-36 sm:mx-16 mx-6 sm:my-3 my-0 text-slate-200 overflow-y-scroll'>
         <Home/>
         <About/>
         <Skill/>
-        <Protfolio/>
+        <Portfolio/>
       </div>
       <Footer/>
     </div>
