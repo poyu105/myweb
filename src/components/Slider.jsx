de462abc49db92dab3b35e13d43080371305a7e6
poyu <chen06115@gmail.com>
2024-12-06 12:44:05 +0800

    新增輪播組件

---
+++ src/components/Slider.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ImageSlider = ({ images }) => {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {images.map((src, index) => (
+          <div key={index}>
+            <img src={src} alt={`Slide ${index + 1}`} className="slider-image rounded" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageSlider;
